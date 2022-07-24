c41414d26bb92d7bda6d946e8971266411b4be52
Clément Sicard <csicard@student.ethz.ch>
2022-07-24 10:44:33 +0200

    added baseline vanilla unet

---
+++ code/models/baselines/baseline_vanilla_unet.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import sys
+
+sys.path.append("..")
+from consts import *
+
+
+class Block(nn.Module):
+    # a repeating structure composed of two convolutional layers with batch normalization and ReLU activations
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(out_ch),
+            nn.Conv2d(
+                in_channels=out_ch, out_channels=out_ch, kernel_size=3, padding=1
+            ),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class UNet(nn.Module):
+    # UNet-like architecture for single class semantic segmentation.
+    def __init__(self, chs=(3, 64, 128, 256, 512, 1024)):
+        super().__init__()
+        enc_chs = chs  # number of channels in the encoder
+        dec_chs = chs[::-1][:-1]  # number of channels in the decoder
+        self.enc_blocks = nn.ModuleList(
+            [Block(in_ch, out_ch) for in_ch, out_ch in zip(enc_chs[:-1], enc_chs[1:])]
+        )  # encoder blocks
+        self.pool = nn.MaxPool2d(
+            2
+        )  # pooling layer (can be reused as it will not be trained)
+        self.upconvs = nn.ModuleList(
+            [
+                nn.ConvTranspose2d(in_ch, out_ch, 2, 2)
+                for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])
+            ]
+        )  # deconvolution
+        self.dec_blocks = nn.ModuleList(
+            [Block(in_ch, out_ch) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])]
+        )  # decoder blocks
+        self.head = nn.Sequential(
+            nn.Conv2d(dec_chs[-1], 1, 1), nn.Sigmoid()
+        )  # 1x1 convolution for producing the output
+
+    def forward(self, x):
+        # encode
+        enc_features = []
+        for block in self.enc_blocks[:-1]:
+            x = block(x)  # pass through the block
+            enc_features.append(x)  # save features for skip connections
+            x = self.pool(x)  # decrease resolution
+        x = self.enc_blocks[-1](x)
+        # decode
+        for block, upconv, feature in zip(
+            self.dec_blocks, self.upconvs, enc_features[::-1]
+        ):
+            x = upconv(x)  # increase resolution
+            x = torch.cat([x, feature], dim=1)  # concatenate skip features
+            x = block(x)  # pass through the block
+        return self.head(x)  # reduce to 1 channel
+
+
+def patch_accuracy_fn(y_hat, y):
+    # computes accuracy weighted by patches (metric used on Kaggle for evaluation)
+    h_patches = y.shape[-2] // PATCH_SIZE
+    w_patches = y.shape[-1] // PATCH_SIZE
+    patches_hat = (
+        y_hat.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean(
+            (-1, -3)
+        )
+        > CUTOFF
+    )
+    patches = (
+        y.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3))
+        > CUTOFF
+    )
+    return (patches == patches_hat).float().mean()
