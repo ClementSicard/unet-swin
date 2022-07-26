99cc5e0bd98852097f46566de1e73a40c0c25e11
Clément Sicard <csicard@student.ethz.ch>
2022-07-27 00:26:34 +0200

    removed absl==0.0

--- run_euler.sh
+++ run_euler.sh
@@ -1,3 +1,8 @@
 cd CI-Lab
 git pull
-bsub -n 8 -W 12:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" ./run.sh
+module load gcc/6.3.0 python_gpu/3.8.5
+pip install torch==1.12.0
+pip install torchvision==0.13.0
+pip install typing_extensions==4.3.0
+TAG=$(git rev-parse --short HEAD)
+bsub -n 8 -W 12:00 -J $TAG -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" ./run.sh
