c253412ec0fb9e35338a30d35ffd983a91904efe
Clément Sicard <csicard@student.ethz.ch>
2022-07-27 00:11:43 +0200

    augment validation set as well

--- code/models/baselines/baseline_vanilla_unet.py
+++ code/models/baselines/baseline_vanilla_unet.py
@@ -107,7 +107,7 @@ def run(train_path: str, val_path: str, test_path: str, n_epochs=35, batch_size=
         device,
         use_patches=False,
         resize_to=(384, 384),
-        augment=False,
+        augment=True,
     )
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset, batch_size=batch_size, shuffle=True
