49186c3bcd01da81cac684710cb8d64aa49f8e26
Clément Sicard <csicard@student.ethz.ch>
2022-07-26 23:31:15 +0200

    added RTX2080Ti instead of Titan

--- run_euler.sh
+++ run_euler.sh
@@ -1,3 +1,3 @@
 cd CI-Lab
 git pull
-bsub -n 8 -W 12:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==TitanRTX]" ./run.sh
+bsub -n 8 -W 12:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" ./run.sh
