f51481d3148c2605602fe44267c975239db6bac5
Clément Sicard <csicard@student.ethz.ch>
2022-07-27 11:23:04 +0200

    modified run script

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
-module load gcc/6.3.0 python_gpu/3.8.5
-/cluster/apps/nss/gcc-6.3.0/python/3.8.5/x86_64/bin/pip install -r requirements.txt
-/cluster/apps/nss/gcc-6.3.0/python/3.8.5/x86_64/bin/pip install --update torchvision
-/cluster/apps/nss/gcc-6.3.0/python/3.8.5/x86_64/bin/pip install --update torchmetrics
+module load gcc/6.3.0 python_gpu/3.8.5 eth_proxy
+python -m venv venv
+source venv/bin/activate
+module load gcc/6.3.0 python_gpu/3.8.5 eth_proxy
+pip3 install -r requirements.txt
+export PYTHONPATH=$PYTHONPATH:~/CI-Lab
 
-/cluster/apps/nss/gcc-6.3.0/python/3.8.5/x86_64/bin/python code/run.py baseline-unet \
+python code/run.py baseline-unet \
     --train-dir "data/training" \
     --test-dir "data/test" \
     --val-dir "data/validation" \
