711e100aacb964923897535bdedde14818719a9e
Clément Sicard <csicard@student.ethz.ch>
2022-07-27 11:17:55 +0200

    fixed run scripts

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 module load gcc/6.3.0 python_gpu/3.8.5
-pip install -r requirements.txt
-pip install --update torchvision
-pip install --update torchmetrics
+/cluster/apps/nss/gcc-6.3.0/python/3.8.5/x86_64/bin/pip install -r requirements.txt
+/cluster/apps/nss/gcc-6.3.0/python/3.8.5/x86_64/bin/pip install --update torchvision
+/cluster/apps/nss/gcc-6.3.0/python/3.8.5/x86_64/bin/pip install --update torchmetrics
 
-python code/run.py baseline-unet \
+/cluster/apps/nss/gcc-6.3.0/python/3.8.5/x86_64/bin/python code/run.py baseline-unet \
     --train-dir "data/training" \
     --test-dir "data/test" \
     --val-dir "data/validation" \
