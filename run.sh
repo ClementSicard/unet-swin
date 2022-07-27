a0b01f0ad87c06dfb5262bb003f433af66fb167e
Clément Sicard <csicard@student.ethz.ch>
2022-07-27 18:05:02 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ python code/run.py swin-unet \
     --test-dir "data/test" \
     --val-dir "data/validation" \
     --n_epochs 35 \
-    --batch_size 4
+    --batch_size 4 \
+    --model-save-dir $SCRATCH
