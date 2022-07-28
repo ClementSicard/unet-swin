5a3470ca98d288f2b17cdd7f31a0981a39beb123
Clément Sicard <csicard@student.ethz.ch>
2022-07-28 19:14:03 +0200

    cleaned baseline unet

--- code/models/unet.py
+++ code/models/unet.py
@@ -1,9 +1,17 @@
-from consts import *
-from utils import *
 import torch
 import torch.nn as nn
+from datetime import datetime
+from train import train
+from dataset import ImageDataset
+from utils import *
+from .losses.mixed_loss import MixedLoss
+from .losses.focal_loss import FocalLoss
+from .losses.dice_loss import BinaryDiceLoss
+from torch import nn
+import torch
 import sys
-sys.path.append("..")
+import numpy as np
+import cv2
 
 
 class Block(nn.Module):
@@ -11,8 +19,7 @@ class Block(nn.Module):
     def __init__(self, in_ch, out_ch):
         super().__init__()
         self.block = nn.Sequential(
-            nn.Conv2d(in_channels=in_ch, out_channels=out_ch,
-                      kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(out_ch),
             nn.Conv2d(
@@ -32,8 +39,7 @@ class UNet(nn.Module):
         enc_chs = chs  # number of channels in the encoder
         dec_chs = chs[::-1][:-1]  # number of channels in the decoder
         self.enc_blocks = nn.ModuleList(
-            [Block(in_ch, out_ch)
-             for in_ch, out_ch in zip(enc_chs[:-1], enc_chs[1:])]
+            [Block(in_ch, out_ch) for in_ch, out_ch in zip(enc_chs[:-1], enc_chs[1:])]
         )  # encoder blocks
         self.pool = nn.MaxPool2d(
             2
@@ -45,8 +51,7 @@ class UNet(nn.Module):
             ]
         )  # deconvolution
         self.dec_blocks = nn.ModuleList(
-            [Block(in_ch, out_ch)
-             for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])]
+            [Block(in_ch, out_ch) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])]
         )  # decoder blocks
         self.head = nn.Sequential(
             nn.Conv2d(dec_chs[-1], 1, 1), nn.Sigmoid()
@@ -60,7 +65,6 @@ class UNet(nn.Module):
             enc_features.append(x)  # save features for skip connections
             x = self.pool(x)  # decrease resolution
         x = self.enc_blocks[-1](x)
-
         # decode
         for block, upconv, feature in zip(
             self.dec_blocks, self.upconvs, enc_features[::-1]
@@ -69,3 +73,121 @@ class UNet(nn.Module):
             x = torch.cat([x, feature], dim=1)  # concatenate skip features
             x = block(x)  # pass through the block
         return self.head(x)  # reduce to 1 channel
+
+
+def run(
+    train_path: str,
+    val_path: str,
+    test_path: str,
+    n_epochs=35,
+    batch_size=4,
+    checkpoint_path=None,
+    augment: bool = False,
+    model_save_dir: str = None,
+    loss: str = "bce",
+):
+    log("Training Vanilla-UNet Baseline...")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # reshape the image to simplify the handling of skip connections and maxpooling
+    train_dataset = ImageDataset(
+        train_path,
+        device,
+        use_patches=False,
+        resize_to=(384, 384),
+        augment=augment,
+    )
+    val_dataset = ImageDataset(
+        val_path,
+        device,
+        use_patches=False,
+        resize_to=(384, 384),
+        augment=augment,
+    )
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True
+    )
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=True
+    )
+    model = UNet().to(device)
+    if loss == "bce":
+        loss_fn = nn.BCELoss()
+    elif loss == "dice":
+        loss_fn = BinaryDiceLoss()
+    elif loss == "mixed":
+        loss_fn = MixedLoss()
+    elif loss == "focal":
+        loss_fn = FocalLoss()
+
+    best_metric_fn = {
+        "patch_f1_score": patch_f1_score_fn,
+    }
+    metric_fns = {
+        "acc": accuracy_fn,
+        "patch_acc": patch_accuracy_fn,
+        "patch_f1_score": patch_f1_score_fn,
+    }
+    optimizer = torch.optim.Adam(model.parameters())
+
+    best_weights_path = train(
+        train_dataloader=train_dataloader,
+        eval_dataloader=val_dataloader,
+        model=model,
+        loss_fn=loss_fn,
+        metric_fns=metric_fns,
+        best_metric_fn=best_metric_fn,
+        checkpoint_path=checkpoint_path,
+        model_save_path=model_save_dir,
+        save_state=True,
+        optimizer=optimizer,
+        n_epochs=n_epochs,
+        model_name="baseline_vanilla_unet",
+    )
+
+    log("Training done!")
+
+    log("Predicting on test set...")
+    # predict on test set
+    test_path = os.path.join(test_path, "images")
+    test_filenames = glob(test_path + "/*.png")
+    test_images = load_all_from_path(test_path)
+    batch_size = test_images.shape[0]
+    size = test_images.shape[1:3]
+    # we also need to resize the test images. This might not be the best ideas depending on their spatial resolution.
+    log("Resizing test images...")
+    test_images = np.stack(
+        [cv2.resize(img, dsize=(384, 384)) for img in test_images], 0
+    )
+    test_images = test_images[:, :, :, :3]
+    test_images = np_to_tensor(np.moveaxis(test_images, -1, 1), device)
+    log("Making predictions...")
+    # Load best model state
+    checkpoint = torch.load(best_weights_path)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    log(f"Loaded best model weights ({best_weights_path})")
+
+    test_pred = [
+        model(t).detach().cpu().numpy() for t in tqdm(test_images.unsqueeze(1))
+    ]
+    test_pred = np.concatenate(test_pred, 0)
+    test_pred = np.moveaxis(test_pred, 1, -1)  # CHW to HWC
+    test_pred = np.stack(
+        [cv2.resize(img, dsize=size) for img in test_pred], 0
+    )  # resize to original shape
+    # Now compute labels
+    test_pred = test_pred.reshape(
+        (-1, size[0] // PATCH_SIZE, PATCH_SIZE, size[0] // PATCH_SIZE, PATCH_SIZE)
+    )
+    test_pred = np.moveaxis(test_pred, 2, 3)
+    test_pred = np.round(np.mean(test_pred, (-1, -2)) > CUTOFF)
+    log(f"Test predictions shape: {test_pred.shape}")
+    now = datetime.now()
+    t = now.strftime("%Y-%m-%d_%H:%M:%S")
+    os.makedirs("submissions", exist_ok=True)
+    create_submission(
+        test_pred,
+        test_filenames,
+        submission_filename=f"./submissions/baseline_unet_submission_{t}.csv",
+    )
+    log(f"Created submission!")
