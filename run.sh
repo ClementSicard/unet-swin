fa30639293e38a7b6724b2ed7ad8542c64265e14
Clément Sicard <csicard@student.ethz.ch>
2022-07-29 12:56:38 +0200

    testing with regular unet

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 source ~/.bashrc
 conda activate cil
 
-python code/run.py swin-unet \
+python code/run.py baseline-unet \
     --train-dir "data/training" \
     --test-dir "data/test" \
     --val-dir "data/validation" \
-    --n_epochs 60 \
+    --n_epochs 40 \
     --batch_size 8 \
     --model-type base \
-    --loss focal \
+    --loss mixed \
     --model-save-dir $SCRATCH
