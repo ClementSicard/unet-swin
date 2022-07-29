83e87442ece59f981ea22ca3c2582fcf4f2883ff
Clément Sicard <csicard@student.ethz.ch>
2022-07-29 09:52:28 +0200

    modified run script

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 source ~/.bashrc
 conda activate cil
 
-python code/run.py baseline-unet \
+python code/run.py swin-unet \
     --train-dir "data/training" \
     --test-dir "data/test" \
     --val-dir "data/validation" \
     --n_epochs 50 \
     --batch_size 4 \
     --loss focal \
+    --model-type base \
     --model-save-dir $SCRATCH
