cc6121d460354ec085831e76687193e6f5f389c9
Clément Sicard <csicard@student.ethz.ch>
2022-07-29 19:17:02 +0200

    added 2 GPUs

--- run_euler.sh
+++ run_euler.sh
@@ -1,4 +1,4 @@
 cd CI-Lab
 git pull
 TAG=$(git rev-parse --short HEAD)
-bsub -n 4 -W 12:00 -J $TAG -B -R "rusage[mem=4096, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8192]" <~/CI-Lab/run.sh
+bsub -n 4 -W 12:00 -J $TAG -B -R "rusage[mem=4096, ngpus_excl_p=2]" <~/CI-Lab/run.sh
