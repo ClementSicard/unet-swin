cdf61d345ccd3335c93d6822806581fd28cd9a60
Clément Sicard <csicard@student.ethz.ch>
2022-07-29 13:39:47 +0200

    run ensemble unet

--- run.sh
+++ run.sh
@@ -1,12 +1,61 @@
 source ~/.bashrc
 conda activate cil
 
-python code/run.py swin-unet \
+N_EPOCHS=40
+BATCH_SIZE=16
+
+echo "1st run"
+python code/run.py baseline-unet \
     --train-dir "data/training" \
     --test-dir "data/test" \
     --val-dir "data/validation" \
-    --n_epochs 40 \
-    --batch_size 8 \
+    --n_epochs $N_EPOCHS \
+    --batch_size $BATCH_SIZE \
+    --loss mixed \
+    --model-save-dir $SCRATCH
+
+echo "2nd run"
+python code/run.py baseline-unet \
+    --train-dir "data/training" \
+    --test-dir "data/test" \
+    --val-dir "data/validation" \
+    --n_epochs $N_EPOCHS \
+    --batch_size $BATCH_SIZE \
+    --model-type base \
+    --loss focal \
+    --model-save-dir $SCRATCH
+
+echo "3rd run"
+python code/run.py baseline-unet \
+    --train-dir "data/training" \
+    --test-dir "data/test" \
+    --val-dir "data/validation" \
+    --n_epochs $N_EPOCHS \
+    --batch_size $BATCH_SIZE \
+    --model-type base \
+    --loss focal \
+    --model-save-dir $SCRATCH
+
+echo "4th run"
+python code/run.py baseline-unet \
+    --train-dir "data/training" \
+    --test-dir "data/test" \
+    --val-dir "data/validation" \
+    --n_epochs $N_EPOCHS \
+    --batch_size $BATCH_SIZE \
     --model-type base \
     --loss mixed \
     --model-save-dir $SCRATCH
+
+echo "5th run"
+python code/run.py baseline-unet \
+    --train-dir "data/training" \
+    --test-dir "data/test" \
+    --val-dir "data/validation" \
+    --n_epochs $N_EPOCHS \
+    --batch_size $BATCH_SIZE \
+    --model-type base \
+    --loss bce \
+    --model-save-dir $SCRATCH
+
+echo "Done!"
