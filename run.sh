9ab084c1fa42c5232b51314dae0d170e104774eb
Clément Sicard <csicard@student.ethz.ch>
2022-07-30 15:11:23 +0200

    new run with swin-unet

--- run.sh
+++ run.sh
@@ -4,22 +4,22 @@ conda activate cil
 N_EPOCHS=50
 BATCH_SIZE=16
 
-python code/run.py unet \
+python code/run.py swin-unet \
+    --train-dir "data/training" \
+    --test-dir "data/test" \
+    --val-dir "data/validation" \
+    --n_epochs $N_EPOCHS \
+    --batch_size $BATCH_SIZE \
+    --loss focal \
+    --model-save-dir $SCRATCH
+
+python code/run.py swin-unet \
     --train-dir "data/training" \
     --test-dir "data/test" \
     --val-dir "data/validation" \
     --n_epochs $N_EPOCHS \
     --batch_size $BATCH_SIZE \
     --loss patch-f1 \
-    --model-save-dir $SCRATCH
-
-python code/run.py unet \
-    --train-dir "data/training" \
-    --test-dir "data/test" \
-    --val-dir "data/validation" \
-    --n_epochs $N_EPOCHS \
-    --batch_size $BATCH_SIZE \
-    --loss f1 \
     --model-save-dir $SCRATCH
 
 echo "Done!"
