2b1d0c860010210e617c400faf76525a69d9beaf
Kajpio <kajetan.pyszkowski@gmail.com>
2022-07-30 19:52:34 +0200

    Update run_euler.sh

--- run_euler.sh
+++ run_euler.sh
@@ -1,4 +1,4 @@
 cd CI-Lab
 git pull
 TAG=$(git rev-parse --short HEAD)
-bsub -n 1 -W 12:00 -J $TAG -B -R "rusage[mem=2048, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8192]" <~/CI-Lab/run.sh
+bsub -n 1 -W 12:00 -J $TAG -B -R "rusage[mem=4096, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8192]" <~/CI-Lab/run.sh
