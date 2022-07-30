c5e6a600459648ccbbcaf4f8ffe641a228f56576
Clément Sicard <csicard@student.ethz.ch>
2022-07-30 09:40:12 +0200

    new run (reduced batch size + changed losses)

--- run.sh
+++ run.sh
@@ -2,17 +2,7 @@ source ~/.bashrc
 conda activate cil
 
 N_EPOCHS=50
-BATCH_SIZE=32
-
-python code/run.py swin-unet \
-    --train-dir "data/training" \
-    --test-dir "data/test" \
-    --val-dir "data/validation" \
-    --n_epochs $N_EPOCHS \
-    --batch_size $BATCH_SIZE \
-    --model-type base \
-    --loss mixed \
-    --model-save-dir $SCRATCH
+BATCH_SIZE=16
 
 python code/run.py swin-unet \
     --train-dir "data/training" \
@@ -22,6 +12,16 @@ python code/run.py swin-unet \
     --batch_size $BATCH_SIZE \
     --model-type base \
     --loss focal \
+    --model-save-dir $SCRATCH
+
+python code/run.py swin-unet \
+    --train-dir "data/training" \
+    --test-dir "data/test" \
+    --val-dir "data/validation" \
+    --n_epochs $N_EPOCHS \
+    --batch_size $BATCH_SIZE \
+    --model-type base \
+    --loss twersky \
     --model-save-dir $SCRATCH
 
 echo "Done!"
