f9210d44cf7ae94695d49d1fadd4b570e68a06b2
Kajpio <kajetan.pyszkowski@gmail.com>
2022-07-30 16:22:41 +0200

    Update swin_unet.py

--- code/models/swin_unet.py
+++ code/models/swin_unet.py
@@ -305,7 +305,7 @@ def test_and_create_sub(
                     for c_img in cropped_image
                 ]
                 # save cropped to file
-                cv2.imwrite("crops/{}.png".format(i), resized_image[0])
+                cv2.imwrite("{}.png".format(i), resized_image[0])
 
                 # create a tensor from the resized patches
                 resized_crops = np.stack(resized_image, 0)
